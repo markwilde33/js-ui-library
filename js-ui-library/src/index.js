e53ba312e0de8a9e061d58bbcb4168c372ff8744
WildeMedia <wildemedia33@gmail.com>
2020-04-14 20:17:19 +0100

    Added dropdown.css

--- js-ui-library/src/index.js
+++ js-ui-library/src/index.js
@@ -1,9 +1,15 @@
 import Tooltip from './ui-project/tooltip';
+import Dropdown from './ui-project/dropdown';
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
 
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
 
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
 
-console.log('testing 1 2 3');
